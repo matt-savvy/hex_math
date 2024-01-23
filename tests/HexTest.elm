972ef087d53f997eaf732d00b1e8dbf67f83b2e4
Matt Savoia <matt@1-800-rad-dude.com>
2024-01-22 23:20:04 -0500

    Update tests, use hex literals

--- tests/HexTest.elm
+++ tests/HexTest.elm
@@ -18,33 +18,33 @@ suite =
                 \n -> Expect.equal (String.length (toHex n)) 2
             , fuzz (intRange 256 4095) "256-4095 is three digits" <|
                 \n -> Expect.equal (String.length (toHex n)) 3
-            , test "10" <|
-                \_ -> Expect.equal "A" (toHex 10)
-            , test "11" <|
-                \_ -> Expect.equal "B" (toHex 11)
-            , test "12" <|
-                \_ -> Expect.equal "C" (toHex 12)
-            , test "13" <|
-                \_ -> Expect.equal "D" (toHex 13)
-            , test "14" <|
-                \_ -> Expect.equal "E" (toHex 14)
-            , test "15" <|
-                \_ -> Expect.equal "F" (toHex 15)
-            , test "24" <|
-                \_ -> Expect.equal "18" (toHex 24)
-            , test "538" <|
-                \_ -> Expect.equal "21A" (toHex 538)
+            , test "0x10" <|
+                \_ -> Expect.equal "A" (toHex 0x0A)
+            , test "0x11" <|
+                \_ -> Expect.equal "B" (toHex 0x0B)
+            , test "0x12" <|
+                \_ -> Expect.equal "C" (toHex 0x0C)
+            , test "0x13" <|
+                \_ -> Expect.equal "D" (toHex 0x0D)
+            , test "0x14" <|
+                \_ -> Expect.equal "E" (toHex 0x0E)
+            , test "0x15" <|
+                \_ -> Expect.equal "F" (toHex 0x0F)
+            , test "0x18" <|
+                \_ -> Expect.equal "18" (toHex 0x18)
+            , test "0x21A" <|
+                \_ -> Expect.equal "21A" (toHex 0x021A)
             ]
         , describe "fromHex"
             [ fuzz (intRange 0 9) "0-9" <|
                 \n -> Expect.equal (fromHex (String.fromInt n)) (Just n)
-            , test "F" <|
-                \_ -> Expect.equal (fromHex "F") (Just 15)
-            , test "0F" <|
-                \_ -> Expect.equal (fromHex "0F") (Just 15)
-            , test "3B" <|
-                \_ -> Expect.equal (fromHex "3B") (Just 59)
-            , test "206" <|
-                \_ -> Expect.equal (fromHex "206") (Just 518)
+            , test "0xF" <|
+                \_ -> Expect.equal (fromHex "F") (Just 0x0F)
+            , test "0x0F" <|
+                \_ -> Expect.equal (fromHex "0F") (Just 0x0F)
+            , test "0x3B" <|
+                \_ -> Expect.equal (fromHex "3B") (Just 0x3B)
+            , test "0x206" <|
+                \_ -> Expect.equal (fromHex "206") (Just 0x0206)
             ]
         ]
